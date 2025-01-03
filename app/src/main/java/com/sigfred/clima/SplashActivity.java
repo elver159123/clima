294e36749cb88513a3fb9ef0f33ca383e26436c0
Andy Yanacallo <andyya2004@gmail.com>
2025-01-03 01:12:18 -0500

    actualizacion

--- app/src/main/java/com/sigfred/clima/SplashActivity.java
+++ app/src/main/java/com/sigfred/clima/SplashActivity.java
@@ -4,9 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.sigfred.clima.Activitis.MainActivity;
-import com.sigfred.clima.R;
 
 public class SplashActivity extends AppCompatActivity {
 
